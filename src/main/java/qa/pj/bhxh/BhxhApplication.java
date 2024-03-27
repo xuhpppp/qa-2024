fcc3b2cdc0c08c1d4b43054e9ec2a9b8de51b439
xuhpppp <doanthephuc.ST@gmail.com>
2024-03-27 23:12:19 +0700

    initial commit

---
+++ src/main/java/qa/pj/bhxh/BhxhApplication.java
@@ -0,0 +1,13 @@
+package qa.pj.bhxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhxhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhxhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qa/pj/bhxh/BhxhApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.pj.bhxh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhxhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
